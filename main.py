87eb1ced4a03a822b4a1c9d251400e8fbcdffb6b
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-07 01:40:37 +0700

    How Python class attributes work

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from circle import Circle
+from test import Test
 
 if __name__ == "__main__":
   # Introduction to class attributes
 
-  print(Circle.__dict__)
-  print(Circle.pi)
+  # print(Circle.__dict__)
+  # print(Circle.pi)
 
-  circle = Circle(10)
-  print(circle.__dict__)
-  print(circle.pi)
-  print(circle.area())
-  print(circle.circumference())
+  # circle = Circle(10)
+  # print(circle.__dict__)
+  # print(circle.pi)
+  # print(circle.area())
+  # print(circle.circumference())
+
+  # How Python class attributes work
+
+  print(Test.x)
+
+  test = Test()
+  print(test.x)
---
+++ test.py
@@ -0,0 +1,5 @@
+class Test:
+  x = 10
+
+  def __init__(self):
+    self.x = 20
